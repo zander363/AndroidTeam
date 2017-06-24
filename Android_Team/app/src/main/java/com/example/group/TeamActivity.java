241ce1e4dcc43b3668e2d608d8366a99d62fbe3e
zander363 <b04505021@ntu.edu.tw>
2017-06-25 03:55:46 +0800

    try to add recyclerview and its adapter

--- Android_Team/app/src/main/java/com/example/group/TeamActivity.java
+++ Android_Team/app/src/main/java/com/example/group/TeamActivity.java
@@ -5,15 +5,20 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
+import android.widget.*;
+import android.support.v7.widget.*;
 
 
 
 
 public class TeamActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] arr=new String[3];
+    arr[0]="87";
+    arr[1]="88";
+    arr[2]="72";
 
     private TextView mTextMessage;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -41,11 +46,17 @@ public class TeamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
+        ArrayAdapter<String> mAdapter = new RecyclerView.Adapter<>(TeamActivity.this,
+                android.R.layout.simple_list_item_1,arr);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 }
