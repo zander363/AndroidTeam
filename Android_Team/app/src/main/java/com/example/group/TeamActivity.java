e765649b79e7111e88e4eb9e2cc6d2986fdaff1d
unknown <>
2017-06-24 20:58:54 +0800

    GUI2.0

--- Android_Team/app/src/main/java/com/example/group/TeamActivity.java
+++ Android_Team/app/src/main/java/com/example/group/TeamActivity.java
@@ -6,25 +6,30 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+
+
 
 public class TeamActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_calendar:
+                    Toast.makeText(TeamActivity.this,"Action calendar Clicked",Toast.LENGTH_SHORT).show();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_group:
+                    Toast.makeText(TeamActivity.this,"Action group Clicked",Toast.LENGTH_SHORT).show();
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_setting:
+                    Toast.makeText(TeamActivity.this,"Action setting Clicked",Toast.LENGTH_SHORT).show();
                     return true;
             }
             return false;
@@ -40,6 +45,7 @@ public class TeamActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
 }
